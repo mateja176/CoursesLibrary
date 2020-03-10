31ecf4bdc2ba21fecfb911606729cd1d91321e5d
mateja <mateja176@gmail.com>
2020-03-10 01:48:56 +0100

    refactor(Models/CourseForCreationDto): adds CourseTitleMustBeDifferentFromDescription attribute

--- Models/CourseForCreationDto.cs
+++ Models/CourseForCreationDto.cs
@@ -1,21 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using CoursesLibrary.ValidationAttributes;
 
 namespace CoursesLibrary.Models
 {
-    public class CourseForCreationDto : IValidatableObject
+    [CourseTitleMustBeDifferentFromDescription]
+    public class CourseForCreationDto
     {
         [Required] [MaxLength(100)] public string Title { get; set; }
         [MaxLength(1500)] public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title == Description)
-            {
-                yield return new ValidationResult(
-                    "The provided description should be different from the title.", new[] {"CourseForCreationDto"});
-            }
-        }
+        // public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        // {
+        //     if (Title == Description)
+        //     {
+        //         yield return new ValidationResult(
+        //             "The provided description should be different from the title.", new[] {"CourseForCreationDto"});
+        //     }
+        // }
     }
 }
