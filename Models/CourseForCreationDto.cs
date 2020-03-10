06168323dc712ddfd6fe17ec8c6405c23d0b0ee0
mateja <mateja176@gmail.com>
2020-03-10 01:43:30 +0100

    refactor(Models/CourseForCreationDto): implements Validate

--- Models/CourseForCreationDto.cs
+++ Models/CourseForCreationDto.cs
@@ -1,10 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoursesLibrary.Models
 {
-    public class CourseForCreationDto
+    public class CourseForCreationDto : IValidatableObject
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
+        [Required] [MaxLength(100)] public string Title { get; set; }
+        [MaxLength(1500)] public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult(
+                    "The provided description should be different from the title.", new[] {"CourseForCreationDto"});
+            }
+        }
     }
 }
